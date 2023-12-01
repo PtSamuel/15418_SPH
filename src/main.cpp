1d46e46c09bd14473a08ccf9b94e7a9a3bb97b6e
John Smith <2695829662@qq.com>
2023-12-01 16:39:45 -0500

    Realized a simple fix...

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <GLFW/glfw3.h>
+
+int main() {
+    // GLFW initialization
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    // Create a windowed mode window and its OpenGL context
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Loop until the user closes the window
+    while (!glfwWindowShouldClose(window)) {
+        // Render here
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    // Clean up
+    glfwTerminate();
+    return 0;
+}
