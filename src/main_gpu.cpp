550181ecdc6f31d62472ad8d6564af9f248b29b8
John Smith <2695829662@qq.com>
2023-12-09 12:47:01 -0500

    Bitonic sort is the bottleneck, use this version for benchmarking

--- src/main_gpu.cpp
+++ src/main_gpu.cpp
@@ -20,7 +20,7 @@
 
 #define PARTICLES 10
 #define PARTICLE_RADIUS 0.1f
-#define PARTICLE_TILE_NUMBER 16
+#define PARTICLE_TILE_NUMBER 256
 #define SAMPLE_TILE_NUMBER 10
 #define OCCUPANCY 0.8f
 #define BOX_WIDTH 10.0f
@@ -537,6 +537,7 @@ static void print_particle(Particle &p) {
 
 static void report_time(Timer &t, const char *str) {
     printf("%s took %f seconds\n", str, t.time());
+    t.reset();
 }
 
 static std::vector<float> densities_ref(particles.size());
@@ -634,18 +635,32 @@ int main() {
 
         set_default();
 
+        Timer timer;
         compute_densities_and_pressures_gpu(particles.size());
+        report_time(timer, "density & pressure");
+
         compute_pressure_grads_newton_gpu(particles.size());
+        report_time(timer, "pressure grad");
+
         compute_x_dot_gpu(particles.size());
+        report_time(timer, "x dot");
 
         step_ahead_gpu(particles.size());  
+        report_time(timer, "step ahead");
+
         set_altered();
 
         compute_densities_and_pressures_gpu(particles.size());
+        report_time(timer, "density & pressure");
+
         compute_pressure_grads_newton_gpu(particles.size());
+        report_time(timer, "pressure grad");
 
         compute_x_dot_gpu(particles.size());
+        report_time(timer, "x dot");
+
         update_particles_gpu(particles.size(), particles.data());
+        report_time(timer, "update particles");
 
         // std::string str;
         // std::getline(std::cin, str);
